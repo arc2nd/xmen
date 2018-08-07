0958999fd15f9b7d4f90b8a9ee6cf3a91e68fc27
James Parks <jparks@wildcanary.com>
2018-08-07 15:20:13 -0700

    better implementation of power list

--- Jubilee.py
+++ Jubilee.py
@@ -16,7 +16,8 @@ class Jubilee(Xman):
         return
 
     def fight(self, how='energy blast'):
-        print('How do you want to fight?\nChoose: Independence Day, Energy Blast, Photo Flash')
+        power_string = ', '.join(self.power_list)
+        print('How do you want to fight?\nChoose: {}'.format(power_string))
         choice = raw_input('> ')
         if 'day' in choice.lower():
             self._independence_day()
